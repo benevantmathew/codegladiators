35775900488e5611fa76d7f3fd96c52c0825709b
benevantmathew <62941107+benevantmathew@users.noreply.github.com>
2020-06-11 00:12:41 +0530

    Create power_puff_girls.py

---
+++ power_puff_girls.py
@@ -0,0 +1,20 @@
+''' Read input from STDIN. Print your output to STDOUT '''
+    #Use input() to read input from STDIN and use print to write your output to STDOUT
+import sys
+import math
+numbs=[]
+for line in sys.stdin.readlines():
+    words=line.split()
+    count=len(words)
+    for x in range(count):
+        numbs=numbs+[int(words[x])]
+N=numbs[0]
+inc=numbs[1:N]
+inv=numbs[N+1:N+N]
+
+x_compare_ini=math.floor(inv[0]/inc[0])
+for x in range(N-1):
+    x_compare=math.floor(inv[x]/inc[x])
+    if x_compare<=x_compare_ini:
+        x_compare_ini=x_compare
+print(x_compare_ini)
