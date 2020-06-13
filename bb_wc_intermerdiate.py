3eda23247082ee80f74a55daabdb4af6c60c8e7c
benevantmathew <62941107+benevantmathew@users.noreply.github.com>
2020-06-14 00:53:14 +0530

    Create bb_wc_intermerdiate.py

---
+++ bb_wc_intermerdiate.py
@@ -0,0 +1,32 @@
+
+def main():    
+    T=int(input())
+
+    for i in range(T):
+
+        N=int(input())
+        GRev_in=input().split()
+        GRev=[]
+        for x in GRev_in:
+            GRev.append(int(x))
+        BB_in=input().split()
+        BB=[]
+        for x in BB_in:
+            BB.append(int(x))    
+
+        #'Sorting the power arrays'
+        GRev.sort()
+        BB.sort()
+
+        #'commencing the battle'
+        
+        count=0
+        for x in BB:
+            for GR_index,y in enumerate(GRev):
+                if x<y:
+                    count+=1
+                    GRev.remove(y)
+                    break
+        print(count)
+    
+main()
